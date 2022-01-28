c8b1721ea270e38001d3935a819984773aa1bcbb
Wilfred <wilfredbayudan@gmail.com>
2022-01-27 22:38:08 -1000

    commit tree 6fd63329001ae19d6dd2d556343cbfbb2394a7bf

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,19 @@
 export function manageFriends(state, action) {
-  // your code here
+  switch (action.type) {
+    case "friends/add":
+      return {
+        ...state,
+        friends: [
+          ...state.friends,
+          action.payload
+        ]
+      }
+    case "friends/remove":
+      return {
+        ...state,
+        friends: state.friends.filter(friend => friend.id !== action.payload)
+      }
+    default:
+      return state;
+  }
 }
